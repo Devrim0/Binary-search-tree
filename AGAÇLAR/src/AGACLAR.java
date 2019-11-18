8389d642632976da1d9fdb641b35d2255d331d34
devrim <56955202+Devrimozcay@users.noreply.github.com>
2019-11-19 01:50:45 +0300

    tree

---
+++ AGAÇLAR/src/AGACLAR.java
@@ -0,0 +1,33 @@
+
+class dugum{
+int icerik;
+dugum sag,sol;
+dugum (int icerik){
+this.icerik=icerik;
+sag=null;
+sol=null;
+}
+}
+public class AGACLAR {
+dugum root;
+public AGACLAR(){
+root=null;
+}
+ void ekle(dugum yeni){
+if(root==null){
+root=yeni;
+}
+else{
+dugum tmp=root;
+dugum once=null;
+while(tmp!=null){
+
+}
+}
+ }
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
